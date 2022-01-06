dec2cfe446f554284db63d9f6ef8606c41f27efe
C <cdperez.85@gmail.com>
2022-01-06 12:02:41 +0100

    tarjetas base y conexion con firebase

---
+++ loaderTarjeta.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeFVAKcmVVMnkTF1Z2EVFSAahdCHKcvjA",
+  authDomain: "monsieurloresuit.firebaseapp.com",
+  projectId: "monsieurloresuit",
+  storageBucket: "monsieurloresuit.appspot.com",
+  messagingSenderId: "617689812422",
+  appId: "1:617689812422:web:f767dc493d6d9e343076c7"
+};
+º
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
